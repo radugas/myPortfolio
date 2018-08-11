e6347f36f90a82dc1e5f434aa5dd85b9945d3c98
Marina <mk.radugasintez@gmail.com>
2018-08-11 14:33:00 +0300

    changed js

--- js/script.js
+++ js/script.js
@@ -20,34 +20,34 @@ $(document).ready(function () {
     });
 
     //form
-    $('.ajax').on('submit', function(){
-        var that = $(this),
-            urlF = that.attr('action'),
-            type = that.attr('method'),
-            data = {};
+    // $('.ajax').on('submit', function(){
+    //     var that = $(this),
+    //         urlF = that.attr('action'),
+    //         type = that.attr('method'),
+    //         data = {};
 
 
-        that.find('[name]').each(function(i, value){
-            var that = $(this),
-                name = that.attr('name'),
-                value = that.val();
-                data[name] = value;
+    //     that.find('[name]').each(function(i, value){
+    //         var that = $(this),
+    //             name = that.attr('name'),
+    //             value = that.val();
+    //             data[name] = value;
 
-        });
+    //     });
 
-        $.ajax({
-            url: urlF, 
-            type: type,
-            data: data,
-            success: function(response){
-                console.log(response);
-            }
-        });
+    //     $.ajax({
+    //         url: urlF, 
+    //         type: type,
+    //         data: data,
+    //         success: function(response){
+    //             console.log(response);
+    //         }
+    //     });
 
         
 
 
-        return false;
-    });
+    //     return false;
+    // });
     
 });
